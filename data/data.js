21eecada81d1519fa22fe6850dbdb33676d2c005
yl-1993 <yl93528@gmail.com>
2019-06-12 10:32:13 +0800

    init webpage

---
+++ data/data.js
@@ -0,0 +1,39 @@
+var data = [
+    {
+        "title": "EDVR",
+        "url_img": "https://xinntao.github.io/images/EDVR.png",
+        "url_paper": "#",
+        "url_src": "#",
+        "url_project": "#"
+    },
+    {
+        "title": "Path-Restore",
+        "url_img": "https://xinntao.github.io/images/path_restore.jpg",
+        "url_paper": "#",
+        "url_src": "#",
+        "url_project": "#"
+    },
+    {
+        "title": "DNI",
+        "url_img": "https://xinntao.github.io/images/DNI.png",
+        "url_paper": "#",
+        "url_src": "#",
+        "url_project": "#"
+    },
+    {
+        "title": "ESRGAN",
+        "url_img": "https://xinntao.github.io/images/esrgan.png",
+        "url_paper": "#",
+        "url_src": "#",
+        "url_project": "#"
+    },
+    {
+        "title": "LTC",
+        "url_img": "http://yanglei.me/project/ltc/imgs/pipeline.png",
+        "url_paper": "#",
+        "url_src": "#",
+        "url_project": "#"
+    },
+];
+// repeat data for testing (uncomment the following line when depolying)
+while (data.length < 100) data = data.concat(data);
